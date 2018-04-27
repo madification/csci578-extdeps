71b37b68da4b7e407d2bdb157ea7716903ef9764
madification <mrburris@usc.edu>
2018-04-27 10:51:49 -0400

    Initial checkin for ImpactHandler is broken; didn't commit early enough.

--- src/FileManipulator/InputInfo.java
+++ src/FileManipulator/InputInfo.java
@@ -24,6 +24,7 @@ public class InputInfo {
         public void populateSlothList(){
             //for every file that is used by another, create a sloth and put it in a list
             this.extDepsMap.forEach((key,set) -> allSloths.put(key, Sloth.createSloth(key, intDepsMap.get(key), set)));
+//            allSloths.computeIfAbsent(this.intDepsMap.forEach((k, s) -> allSloths.put(k, Sloth.createSloth(k, this.intDepsMap.get(k), null))) );
         } //TODO should add a computeIfAbsent on the intDepsList to make sure, if any files are not used by another, they still make it into this list
     }
 
--- src/Infrastructure/ImpactHandler.java
+++ src/Infrastructure/ImpactHandler.java
@@ -3,6 +3,7 @@ package Infrastructure;
 import FileManipulator.InputInfo;
 
 import java.util.HashSet;
+import java.util.PriorityQueue;
 import java.util.Set;
 
 public class ImpactHandler {
@@ -13,9 +14,24 @@ public class ImpactHandler {
 
     public int calCascadingExtDeps(Sloth sloth) {
         Set<String> toAdd = new HashSet<>();
+        PriorityQueue<Sloth> slothQueue = new PriorityQueue<>();
         int total = sloth.usageScore;
+
+        // create a list of all the usages without any duplicates (remove circular dependencies)
+        // go through the list of external dependencies (file name strings) for each file (sloth) and go get the sloth
+        // which matches that string file name
+        sloth.extDepsList.forEach(xDfile -> toAdd.add(inputInfo.allSloths.get(xDfile).fileName));
+        sloth.extDepsList.forEach(xDfile -> slothQueue.add(inputInfo.allSloths.get(xDfile));
+        while(!slothQueue.isEmpty()){
+            Sloth nextSloth = slothQueue.remove();
+            if(!toAdd.contains(nextSloth)){
+                nextSloth.extDepsList.forEach(xDfile -> toAdd.add(inputInfo.allSloths.get(xDfile).fileName));
+            }
+
+        }
+
         // for every sloth in extDepsList, add the usageScore
-        sloth.extDepsList.forEach(s -> toAdd.);
+
 
         // for every sloth in toAdd, add the usage score to total
         toAdd.forEach(s -> total += inputInfo.allSloths.get(s).usageScore);
